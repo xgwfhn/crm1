2e103550315e349383ad75cc5f8b6ed8f9fdca57
xgwfhn@126.com <xgwfhn@126.com>
2016-08-30 11:59:00 +0800

    添加表格按钮图标

--- crm-project-controller/src/main/java/com/xgw/crm/controller/demo/DemoController.java
+++ crm-project-controller/src/main/java/com/xgw/crm/controller/demo/DemoController.java
@@ -2,16 +2,19 @@ package com.xgw.crm.controller.demo;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.xgw.crm.service.file.FileService;
+
 
 @Controller
 @RequestMapping(value = "/demo")
 public class DemoController {
-	
+	private static Logger logger = Logger.getLogger(DemoController.class);
 	@RequestMapping(value = "/start", method = RequestMethod.GET)
 	public String index(HttpServletRequest request, Model model) {
 		System.out.println("index14444");
@@ -19,10 +22,11 @@ public class DemoController {
 	}
 
 	
-	@RequestMapping(value = "/adminIndex", method = RequestMethod.GET)
-	public String adminIndex(HttpServletRequest request, Model model) {
-		System.out.println("index14444");
-		return "adminIndex"; 
+	@RequestMapping(value = "/table", method = RequestMethod.GET)
+	public String table(HttpServletRequest request, Model model) {
+		logger.info("---------------table");
+		logger.info("--------------22222table");
+		return "table/table"; 
 	}
 
 	
